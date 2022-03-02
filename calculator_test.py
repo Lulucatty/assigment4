c2290a06538247fae38e8d8c5d73cb910a66bdad
lulucatty <rashokedwil9@gmail.com>
2022-03-02 06:56:01 -0800

    we r here

--- calculator_test.py
+++ calculator_test.py
@@ -8,6 +8,19 @@ class Test(unittest.TestCase):
     def test_0_add(self):
         result = self.calculator.add(4,8)
         self.assertEqual(result,12)
+
+    def test_1_subtract(self):
+        result = self.calculator.subtract(12,4)
+        self.assertEqual(result,8)
+
+    def test_2_multiply(self):
+        result = self.calculator.multiply(4,8)
+        self.assertEqual(result,32)
+
+    def test_3_add(self):
+        result = self.calculator.divide(8,4)
+        self.assertEqual(result,2)
+
 if __name__ == '__main__':
     # begin the unittest.main()
     unittest.main()
